9bd63731d605d200ddb5b59d532c636c7474edee
Etelvina Oliveira <62815978+teteoliveira@users.noreply.github.com>
2022-07-01 23:44:33 -0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,158 +1,82 @@
-#include <iostream>
+#include "../include/Animal.hpp"
+#include "../include/Herpestidae.hpp"
+#include "../include/Ursidae.hpp"
+#include "../include/Cuidador.hpp"
+#include "../include/Alimentacao.hpp"
 
-#include "Animal.hpp"
-#include "Herpestidae.hpp"
-#include "Ursidae.hpp"
-#include "Cuidador.hpp"
-#include "Alimentacao.hpp"
-using namespace std;
+#include <vector>
 
-int main()
-{
+int main(){
+  std::vector<Animal*> animais;
 
-    Animal animal1;
-    animal1.family = "Ursidae";
-    animal1.nome ="Bobby";
-	animal1.IDADE = "13";
-    animal1.print();
-double kgPeixeConsumidos01 = 0;
-    Animal animal2;
-    animal2.family = "Ursidae";
-    animal2.nome ="Jorge";
-	animal2.IDADE = "1";
-    animal2.print();
+  animais.push_back(new Animal("Bobby", 13,"Ursidae"));
+  animais.push_back(new Animal("Jorge", 1, "Ursidae"));
+  animais.push_back(new Animal("Bonina", 7, "Herpestidae"));
+  animais.push_back(new Animal("Felício", 3, "Ursidae"));
 
-    Animal animal3;
-    animal3.family = "Herpestidae";
-    animal3.nome ="Bonina";
-	animal3.IDADE = "7";
-    animal3.print();
-
-    Animal animal4;
-    animal4.family = "Ursidae";
-    animal4.nome ="Felício";
-	animal4.IDADE = "3";
-    animal4.print();
-
-
-
-    Ursidae e02;
-    int numAtendimentos02 = 0;
-    double comissao02 = 0;
-    e02.nome = "Tony";
-    e02.IDADE = 9;
-    e02.especie = "Urso Pardo";
-    e02.kgPeixePorDIA = 10.5;
-  e02.print();
-
-
-    Ursidae e03;
-    int numAtendimentos03 = 0;
-    double comissao03 = 0;
-    e03.nome = "Wanda";
-    e03.IDADE = 10;
-    e03.especie = "Urso Polar";
-    e03.kgPeixePorDIA = 7;
-  e03.print();
-
-
-    Herpestidae g01;
-    g01.family = "Herpestidae";
-    g01.nome ="Catita";
-	g01.IDADE = "2";
-  g01.species = "Suricato";
-g01.print();
-    Herpestidae g02;
-    double bonificacao01 = 0;
-    g02.family = "Herpestidae";
-    g02.nome ="Carolina";
-	g02.IDADE = "2";
-  g02.species = "Suricato";
-
-double kgPeixeConsumidos02 = 0;
-double kgPeixeConsumidos03 = 0;
-double kgPeixeConsumidos04 = 0;
-double kgPeixeConsumidos05 = 0;
-
-double kgRacaoConsumidos01 = 0;
-double kgRacaoConsumidos02 = 0;
-double kgRacaoConsumidos03 = 0;
+  for (Animal* animal : animais){
+    animal->print();
+  }
   
-  Cuidador cuid01;
-  cuid01.NOME = "Ana Maria Rodrigues Lopes";
-	cuid01.cpf = "1234567799";
-	cuid01.Telefone = "(31) 99876-6923";
-	cuid01.BIRTH = "10/10/2000";
-  cuid01.salario = "R$ 3000";
+  std::vector<Ursidae*> ursidae;
 
-  Cuidador cuid2;
-  cuid2.NOME = "Fernanda Silva Santos";
-	cuid2.cpf = "9472567121";
-	cuid2.Telefone = "(31) 99123-8970";
-	cuid2.BIRTH = "08/02/1995";
+  ursidae.push_back(new Ursidae("Tony", 9, "Urso Pardo", 10.5));
+  ursidae.push_back(new Ursidae("Wanda", 10, "Urso Polar", 7));
 
-cuid2.salario = "R$ 4000";
-    Alimentacao v01;
-    v01.porcao= 2;
-    v01.comida = "Peixes";
-    v01.descricao = "Bobby só quis comer 2 porções de peixes hoje";
-    v01.cuid = cuid01;
-    v01.nomeAnimal = "Bobby";
-    kgPeixeConsumidos01 = 5*2;
-    Alimentacao v02;
-    v02.porcao= 1;
-    v02.comida = "Ração especial";
-    v02.descricao = "Catita comeu direitinho";
-    v02.cuid = cuid2;
-    v02.nomeAnimal = "Catita";
-  kgRacaoConsumidos01 += g01.kgConsumidosDeRacao(v02.porcao);
+  for (Ursidae* animal : ursidae){
+    animal->print();
+  }
 
- Alimentacao v03;
-    v03.porcao= 3;
-    v03.comida = "Ração";
-    v03.descricao = "Carolina estava com um apetite e tanto";
-    v03.cuid = cuid2;
-    v03.nomeAnimal = "Carolina";
-  kgRacaoConsumidos02 += g02.kgConsumidosDeRacao(v03.porcao);
+  std::vector<Herpestidae*> herpestidae;
 
-  Alimentacao v04;
-    v04.porcao= 10;
-    v04.comida = "Peixes";
-    v04.descricao = "Tony comeu um pouco da comida do Bobby";
-    v04.cuid = cuid01;
-    v04.nomeAnimal = "Tony";
-    v02.porcao = 2;
-    v03.comida = "Ração";
-kgPeixeConsumidos03 = 
-  e02.calcula_PEIXES_CONSUMIDOS(10);
+  herpestidae.push_back(new Herpestidae("Catita", 2, "Suricato"));
+  herpestidae.push_back(new Herpestidae("Wanda", 10, "Suricato"));
+
+  herpestidae[0]->print();
+
+  std::vector<Cuidador*> cuidadores;
+  cuidadores.push_back(new Cuidador("Ana Maria Rodrigues Lopes", "1234567799", "(31) 99876-6923",
+    "10/10/2000", 3000));
+  cuidadores.push_back(new Cuidador("Fernanda Silva Santos", "9472567121", "(31) 99123-8970",
+    "08/02/1995", 4000));
+
+  std::vector<Alimentacao*> alimentacaoAnimais;
+  alimentacaoAnimais.push_back(new Alimentacao(2, "Peixes", "Bobby só quis comer 2 porções de peixes hoje",
+    *cuidadores[0], "Bobby"));
+  alimentacaoAnimais.push_back(new Alimentacao(1, "Ração especial", "Catita comeu direitinho",
+    *cuidadores[1], "Catita"));
+  alimentacaoAnimais.push_back(new Alimentacao(3, "Ração", "Carolina estava com um apetite e tanto",
+    *cuidadores[1], "Carolina"));
+  alimentacaoAnimais.push_back(new Alimentacao(10, "Peixes", "Tony comeu um pouco da comida do Bobby",
+    *cuidadores[0], "Tony"));
+
+  alimentacaoAnimais[1]->setPorcao(2);
+  alimentacaoAnimais[2]->setComida("Ração");
+
+  int kgPeixeConsumidos01 = ursidae[1]->calculaPeixesConsumidos(2);
+  int kgRacaoConsumidos01 = herpestidae[0]->kgConsumidosDeRacao(alimentacaoAnimais[1]->getPorcao());
+  int kgRacaoConsumidos02 = herpestidae[1]->kgConsumidosDeRacao(alimentacaoAnimais[1]->getPorcao());
+  int kgPeixeConsumidos02 = ursidae[0]->calculaPeixesConsumidos(10);
 
 
+  std::cout <<" \n \n--------------------------------\n\nRelatorio das alimentações \n" << std::endl;
 
-    cout <<" \n \n--------------------------------\n\nRelatorio das alimentações \n" << endl;
+  for (Alimentacao* item : alimentacaoAnimais){
+    item->print();
+    std::cout << " Descricao: " << item->getDescricao() << "\n" << std::endl;
+  }
 
-    v01.print();
-    cout << " Descricao: " << v01.descricao << endl << endl;
+  std::cout <<" \n \n--------------------------------\n\nRelatorio de kg de comida gastos \n" << std::endl;
+  std::cout << "Tipo de comida: " << "Ração" << std::endl;
+  std::cout << "Kg consumidos: " << kgRacaoConsumidos01 + kgRacaoConsumidos02<<std::endl;
+  std::cout << "\nTipo de comida: " << "Peixe" << std::endl;
+  std::cout << "Kg consumidos: " << kgPeixeConsumidos01 + kgPeixeConsumidos02<<std::endl;
 
-    v02.print();
-    cout << " Descricao: " << v02.descricao << endl<<endl;
+  animais.clear();
+  ursidae.clear();
+  herpestidae.clear();
+  cuidadores.clear();
+  alimentacaoAnimais.clear();
 
-    v03.print();
-    cout << " Descricao: " << v03.descricao << endl<< endl;
-
-    v04.print();
-    cout << " Descricao: " << v04.descricao << endl << "\n";
-
-
-
-
-
-
-    cout <<" \n \n--------------------------------\n\nRelatorio de kg de comida gastos \n" << endl;
-    cout << "Tipo de comida: " << "Ração" << endl;
-    cout << "Kg consumidos: " << kgRacaoConsumidos01+kgRacaoConsumidos02<<endl;
-
-    cout << "\nTipo de comida: " << "Peixe" << endl;
-    cout << "Kg consumidos: " << kgPeixeConsumidos01+kgPeixeConsumidos03<<endl;
-
-    return 0;
+  return 0;
 }
