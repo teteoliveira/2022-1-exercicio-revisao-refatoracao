40288f22d30fa6a849eadb3515bea990ba92f68e
Etelvina Oliveira <62815978+teteoliveira@users.noreply.github.com>
2022-07-01 23:41:44 -0300

    Update Animal.hpp

--- Animal.hpp
+++ Animal.hpp
@@ -4,25 +4,15 @@
 #include <iostream>
 #include <iomanip>
 
-using namespace std;
-
 class Animal {
-    public:
-        string IDADE;
-        string family;
-        string nome;
-
-        void print() {
-            cout << "\n[Animal]" << endl
-            << "  Nome: " << nome << endl
-
-              << "  Idade: " << IDADE << endl
-            << "  Familia: " << family << endl;
-        }
-
-        void print_oi(){
-            cout << "Tchau" << endl;
-        }
+  protected:
+    std::string nome;
+    int idade;
+    std::string familia;
+  
+  public:
+    Animal(std::string _nome, int _idade, std::string _familia);
+    virtual void print();
 };
 
 #endif
