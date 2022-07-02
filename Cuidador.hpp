ebf349df42592f635f8fe7aa4527dd269edc4e02
Etelvina Oliveira <62815978+teteoliveira@users.noreply.github.com>
2022-07-01 23:43:04 -0300

    Update Cuidador.hpp

--- Cuidador.hpp
+++ Cuidador.hpp
@@ -1,21 +1,24 @@
-#ifndef Cuidador_H
-#define Cuidador_H
+#ifndef CUIDADOR_HPP
+#define CUIDADOR_HPP
 
+#include <iostream>
 #include <string>
-using namespace std;
 
 class Cuidador{
-
+	private:
+		std::string nome;
+		std::string cpf;
+		std::string dataDeNascimento;
+		std::string telefone;
+    double salario;
+			
 	public:
+		Cuidador(std::string _nome, std::string _cpf, std::string _dataDeNascimento, 
+			std::string _telefone, double _salario); 	
+			
+		std::string getNome();
 
-    string salario;
-		string NOME;
-		string cpf;
-		string Telefone;
-		string BIRTH;
-
-		void print(); // imprime na tela os dados de um cliente cadastrado
-
+		void print(); 
 };
 
 #endif
